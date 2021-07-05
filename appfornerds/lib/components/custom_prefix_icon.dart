f2d49fa734bd34ae0d3f655c44ee6fac103332f4
prabin-sr <prabin.s106@gmail.com>
2021-07-05 23:08:49 +0530

    SignIn and SignUp Added

---
+++ appfornerds/lib/components/custom_prefix_icon.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+
+import '../size_config.dart';
+
+class CustomPrefixIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: getProportionateScreenWidth(120),
+      child: CountryCodePicker(
+          onChanged: print,
+          // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+          initialSelection: 'IN',
+          // optional. Shows only country name and flag
+          showCountryOnly: true,
+          // optional. Shows only country name and flag when popup is closed.
+          showOnlyCountryWhenClosed: false,
+          // optional. aligns the flag and the Text left
+          alignLeft: true,
+          hideSearch: true,
+          textStyle: TextStyle(color: Colors.black)),
+    );
+  }
+}
---
+++ appfornerds/lib/helper/dimension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Dimensions {
+  static double boxWidth;
+  static double boxHeight;
+
+  Dimensions(context) {
+    boxHeight = MediaQuery.of(context).size.height / 100;
+    boxWidth = MediaQuery.of(context).size.width / 100;
+  }
+}
